920dd68b81b8a1e7592af9ea22b539effeb65124
daihaoxin <daihaoxin@126.com>
2022-09-17 23:58:28 +0800

    test(其他): add test suit

---
+++ __tests__/JSXButton.test.ts
@@ -0,0 +1,20 @@
+import JSXButton from "../src/JSXButton";
+import { mount } from "@vue/test-utils";
+
+describe("Test JSXButton", () => {
+  test("content", () => {
+    const Comp = {
+      template: `<div><JSXButton></JSXButton></div>`,
+    };
+    const wrapper = mount(Comp, {
+      global: {
+        components: {
+          JSXButton,
+        },
+      },
+    });
+    expect(wrapper.findComponent({ name: JSXButton.name }).text()).toContain(
+      "JSX"
+    );
+  });
+});
